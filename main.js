3710cdc8d92bc0c5afa1460405ee34d3e549447e
gmarvis <sgmarvis@gmail.com>
2023-08-17 13:19:14 +0100

    js lintax

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const word = document.querySelector("#word");
-const btn = document.querySelector("#btn");
+const word = document.querySelector('#word')
+const btn = document.querySelector('#btn')
 // results = document.getElementById('results')
 
 // btn.addEventListener('click', function(){
@@ -7,43 +7,42 @@ const btn = document.querySelector("#btn");
 
 // })
 
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
+btn.addEventListener('click', function (e) {
+  e.preventDefault()
 
   // we first prevent the submition of an empty string
 
-  if (word.value === "") {
-    document.querySelector("span").innerText = "please enter a word or phrase";
-    document.querySelector("span").style.color = "red";
-    return;
+  if (word.value === '') {
+    document.querySelector('span').innerText = 'please enter a word or phrase'
+    document.querySelector('span').style.color = 'red'
+    return
   }
   // convert string to lowercase
   // split the srinng after every latter
   // remove all spaces
-  const wordrem = word.value.toLowerCase().split(" ").join("");
+  const wordrem = word.value.toLowerCase().split(' ').join('')
 
-  console.log(wordrem);
+  console.log(wordrem)
 
   // invert the string so we can compare the with previews to check palindriome
   const resersed = word.value
     .toLowerCase()
-    .split(" ")
-    .join("")
-    .split("")
+    .split(' ')
+    .join('')
+    .split('')
     .reverse()
-    .join("");
-  console.log(resersed);
+    .join('')
+  console.log(resersed)
 
   // our first condition is to check is our main string or sentence in equal the reversed string.
   if (resersed === wordrem) {
-    document.querySelector("span").innerText = `${word.value} : Is Palindrome`;
-    document.querySelector("span").style.color = "green";
-    return;
-  } else 
-  {
-    document.querySelector("span").innerText = `${word.value} : not Palindrome`;
+    document.querySelector('span').innerText = `${word.value} : Is Palindrome`
+    document.querySelector('span').style.color = 'green'
+    return
+  } else {
+    document.querySelector('span').innerText = `${word.value} : not Palindrome`
   } // we print the the dom our results
-  document.querySelector("span").style.color = "red";
+  document.querySelector('span').style.color = 'red'
 
-  word.value = "";
-});
+  word.value = ''
+})
